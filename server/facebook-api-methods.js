4bfa456980638c8c2ab031d303ba0d97290cb7cc
Angela Zhang <zhangela@mit.edu>
2015-05-24 16:10:45 -0700

    facebook photos api: added offset, limit, date

--- server/facebook-api-methods.js
+++ server/facebook-api-methods.js
@@ -1,7 +1,11 @@
 var fbgraph = Meteor.npmRequire("fbgraph");
 
 Meteor.methods({
-  getRecentPhotos: function () {
+  getRecentPhotos: function (numDays, limit, offset) {
+    numDays = numDays || 30;  // default to photos from last 30 days
+    limit = limit || 100;  // default to 100 photos max
+    offset = offset || 0;  // default to offset = 0
+
     if (! Meteor.userId()) {
       throw new Meteor.Error("must-be-logged-in");
     }
@@ -10,9 +14,14 @@ Meteor.methods({
     var id = Meteor.user().services.facebook.id;
 
     var now = moment();
-    var monthAgo = now.subtract({months: 1});
+    var startTime = now.subtract({days: numDays});
 
-    var photos = Meteor.wrapAsync(fbgraph.get)("/" + id + "/photos/?since=" + monthAgo.unix(), {access_token: accessToken});
+    var photosUrl = "/" + id + "/photos/" +
+      "?since=" + startTime.unix() +
+      "&limit=" + limit +
+      "&offset=" + offset;
+
+    var photos = Meteor.wrapAsync(fbgraph.get)(photosUrl, {access_token: accessToken});
 
     Meteor.users.update(Meteor.userId(), {$set: {
       "profile.photos": photos
